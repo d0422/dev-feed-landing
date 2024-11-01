a6ae7e02856e4d8dbc7dc2c43a081959f52b2a1f
d0422 <rlfehd2013@naver.com>
2024-11-01 17:47:50 +0900

    feat: MainSection 추가

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MainSection from './sections/MainSection';
+
+function App() {
+  return (
+    <div className="bg-[#121212CC] w-full h-full font-pretendard text-white items-center justify-center flex flex-col">
+      <MainSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/sections/MainSection.tsx
@@ -0,0 +1,34 @@
+import SectionLayout from '../components/SectionLayout';
+import StoreButton from '../components/StoreButton';
+
+export default function MainSection() {
+  return (
+    <SectionLayout full>
+      <div className="flex justify-evenly items-center w-[80%] mobile:gap-4 mobile:flex-col-reverse">
+        <div className=" h-full items-start flex flex-col justify-center gap-6 mobile:items-center">
+          <div className="font-bold text-[40px]  mobile:text-[27px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
+            <div>간편한 개발 블로그 구독 서비스</div>
+            <div>Dev-Feed</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div>개발 블로그, 이제 한 눈에 확인하고, 구독해보세요.</div>
+            <div>Dev-Feed로 나만의 기술블로그 피드를 만들어봐요.</div>
+          </div>
+          <div className="flex gap-4">
+            <StoreButton
+              href="https://play.google.com/apps/testing/com.devfeed"
+              src="googlePlay.png"
+              name="Google Play"
+            />
+            <StoreButton
+              href={'mailto:rlfehd2013@naver.com'}
+              src="appStore.png"
+              name="Apple Store"
+            />
+          </div>
+        </div>
+        <img src="/main.png" className="w-1/4 min-w-32" />
+      </div>
+    </SectionLayout>
+  );
+}
