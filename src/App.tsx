e242d50c9d8b5faa8e02f86b93d7fb79311f34d7
d0422 <rlfehd2013@naver.com>
2024-11-08 17:37:08 +0900

    chore: SubscribeSectoin, IntroSection 순서변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="w-full h-full font-pretendard text-white items-center justify-center flex flex-col">
       <MainSection />
-      <IntroSection />
       <SubscribeSection />
+      <IntroSection />
     </div>
   );
 }
