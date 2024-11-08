60f697d8557c0191c61a61d30e1d8b90a497aaac
d0422 <rlfehd2013@naver.com>
2024-11-08 09:41:01 +0900

    feat: MainSection showArrow 적용

--- src/sections/MainSection.tsx
+++ src/sections/MainSection.tsx
@@ -3,10 +3,10 @@ import StoreButton from '../components/StoreButton';
 
 export default function MainSection() {
   return (
-    <SectionLayout full>
+    <SectionLayout full showArrow>
       <div className="flex justify-evenly items-center w-[80%] mobile:gap-4 mobile:flex-col-reverse">
         <div className=" h-full items-start flex flex-col justify-center gap-6 mobile:items-center">
-          <div className="font-bold text-[40px]  mobile:text-[20px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
+          <div className="font- text-[40px]  mobile:text-[20px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
             <div>로그인 없는</div>
             <div>간편한 개발 블로그 구독 서비스</div>
             <div>Dev-Feed</div>
