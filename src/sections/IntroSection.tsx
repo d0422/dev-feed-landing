ff7353bf19998349a5359481ccc5fd08032fcb24
d0422 <rlfehd2013@naver.com>
2024-11-08 09:59:17 +0900

    chore: IntroSection 반응형 글꼴 적용

--- src/sections/IntroSection.tsx
+++ src/sections/IntroSection.tsx
@@ -18,7 +18,7 @@ export default function IntroSection() {
             <div className="font-bold text-[40px]  mobile:text-[20px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
               <div>나만의 개발 블로그 피드</div>
             </div>
-            <div className="flex flex-col gap-1 mobile:items-center text-[20px]">
+            <div className="flex flex-col gap-1 mobile:items-center text-[20px] mobile:text-[16px]">
               <div>직접 구독한 블로그로만 구성된 피드를 </div>
               <div>살펴보고 편하게 읽어봐요.</div>
             </div>
