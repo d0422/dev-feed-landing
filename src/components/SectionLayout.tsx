a23eb66da2ded5fbf870134d710a6a288f30bba2
d0422 <rlfehd2013@naver.com>
2024-11-08 09:39:35 +0900

    feat: SectionLayout에 Arrow 추가

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -1,20 +1,35 @@
 import { PropsWithChildren } from 'react';
+import Arrow from './Arrow';
 
 interface SectionLayoutProps {
   full?: boolean;
+  showArrow?: boolean;
 }
 
 export default function SectionLayout({
   children,
   full,
+  showArrow,
 }: PropsWithChildren<SectionLayoutProps>) {
+  const scroll = () => {
+    window.scrollBy({
+      top: 1,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div
-      className={`flex justify-center items-center py-12 ${
+      className={`flex justify-center items-center py-12 snap-start flex-col relative overflow-hidden ${
         full ? 'min-h-[100vh]' : 'h-full'
       }`}
     >
       {children}
+      {showArrow && (
+        <button className="absolute bottom-6 animate-bounce" onClick={scroll}>
+          <Arrow />
+        </button>
+      )}
     </div>
   );
 }
