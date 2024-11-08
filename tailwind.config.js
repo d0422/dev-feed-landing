b7da281ae5883f90d5e363e633497438ffa041ad
d0422 <rlfehd2013@naver.com>
2024-11-08 09:40:28 +0900

    chore: animation 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      keyframes: {
+        fadeInLeft: {
+          '0%': { transform: 'translate(-100%)', opacity: 0 },
+          '100%': { transform: 'translate(0%)', opacity: 1 },
+        },
+        fadeOutLeft: {
+          '0%': { transform: 'translate(0%)', opacity: 1 },
+          '100%': { transform: 'translate(-100%)', opacity: 0 },
+        },
+        fadeInRight: {
+          '0%': { transform: 'translate(100%)', opacity: 0 },
+          '100%': { transform: 'translate(0%)', opacity: 1 },
+        },
+        fadeOutRight: {
+          '0%': { transform: 'translate(0%)', opacity: 1 },
+          '100%': { transform: 'translate(100%)', opacity: 0 },
+        },
+      },
+      animation: {
+        fadeInLeft: 'fadeInLeft 500ms linear ',
+        fadeOutLeft: 'fadeOutLeft 500ms linear forwards',
+        fadeInRight: 'fadeInRight 500ms linear ',
+        fadeOutRight: 'fadeOutRight 500ms linear forwards',
+      },
       fontFamily: {
         pretendard: 'Pretendard-Regular',
       },
