4758ba7eda0f5af35f898e46182ea1438447c0ed
d0422 <rlfehd2013@naver.com>
2024-11-13 02:37:48 +0900

    chore: MainSection 문구 간소화

--- src/sections/MainSection.tsx
+++ src/sections/MainSection.tsx
@@ -7,14 +7,10 @@ export default function MainSection() {
     <SectionLayout full showArrow>
       <div className="flex justify-evenly items-center w-[80%] mobile:gap-4 mobile:flex-col-reverse">
         <div className=" h-full items-start flex flex-col justify-center gap-6 mobile:items-center">
-          <div className="font- text-[40px]  mobile:text-[20px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
+          <div className="text-[50px]">Dev-Feed</div>
+          <div className="font- text-[25px]  mobile:text-[20px] tablet:text-[30px]  gap-1 flex flex-col mobile:items-center">
             <div>로그인 없는</div>
             <div>간편한 개발 블로그 구독 서비스</div>
-            <div>Dev-Feed</div>
-          </div>
-          <div className="flex flex-col gap-2 mobile:text-[14px]">
-            <div>개발 블로그, 이제 한 눈에 확인하고, 구독해보세요.</div>
-            <div>Dev-Feed로 나만의 기술블로그 피드를 만들어봐요.</div>
           </div>
           <div className="flex gap-4">
             <StoreButton
