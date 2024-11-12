45f6bc4c865bb4e6d3a41c29492451fd215efb56
d0422 <rlfehd2013@naver.com>
2024-11-13 02:37:16 +0900

    feat: IntersectionObserver를 통한 useFocus훅 구현

---
+++ src/hooks/useFocus.ts
@@ -0,0 +1,46 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+interface UseFocusParams {
+  onFocusCallback?: (() => void) | (() => Promise<void>);
+  onFocusOutCallback?: (() => void) | (() => Promise<void>);
+  threshold?: number;
+  rootMargin?: string;
+}
+
+export default function useFocus<T extends HTMLElement>({
+  onFocusCallback,
+  onFocusOutCallback,
+  threshold,
+  rootMargin,
+}: UseFocusParams) {
+  const elementRef = useRef<T>(null);
+
+  const handleScroll: IntersectionObserverCallback = useCallback(([entry]) => {
+    const { current } = elementRef;
+    if (current) {
+      if (entry.isIntersecting) {
+        onFocusCallback && onFocusCallback();
+      } else {
+        onFocusOutCallback && onFocusOutCallback();
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    let observer: IntersectionObserver;
+    const { current } = elementRef;
+
+    if (current) {
+      observer = new IntersectionObserver(handleScroll, {
+        threshold: threshold || 0.1,
+        rootMargin: rootMargin || '0px 0px 0px 0px',
+      });
+
+      observer.observe(current);
+
+      return () => observer && observer.disconnect();
+    }
+  }, [handleScroll]);
+
+  return elementRef;
+}
