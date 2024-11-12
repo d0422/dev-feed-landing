69dbd12853c881ba50c6398203f24918d26cd26c
d0422 <rlfehd2013@naver.com>
2024-11-13 02:36:40 +0900

    feat: SectionLayout에서 반응형 처리

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -1,4 +1,9 @@
-import { PropsWithChildren } from 'react';
+import {
+  ForwardedRef,
+  forwardRef,
+  MutableRefObject,
+  PropsWithChildren,
+} from 'react';
 import Arrow from './Arrow';
 
 interface SectionLayoutProps {
@@ -6,30 +11,38 @@ interface SectionLayoutProps {
   showArrow?: boolean;
 }
 
-export default function SectionLayout({
-  children,
-  full,
-  showArrow,
-}: PropsWithChildren<SectionLayoutProps>) {
-  const scroll = () => {
-    window.scrollBy({
-      top: 1,
-      behavior: 'smooth',
-    });
-  };
+const SectionLayout = forwardRef(
+  (
+    { children, full, showArrow }: PropsWithChildren<SectionLayoutProps>,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    const scroll = () => {
+      window.scrollBy({
+        top: 1,
+        behavior: 'smooth',
+      });
+    };
 
-  return (
-    <div
-      className={`flex justify-center items-center py-12 snap-end flex-col relative overflow-hidden ${
-        full ? 'min-h-[100svh]' : 'h-full'
-      }`}
-    >
-      {children}
-      {showArrow && (
-        <button className="absolute bottom-6 animate-bounce" onClick={scroll}>
-          <Arrow />
-        </button>
-      )}
-    </div>
-  );
-}
+    return (
+      <div className="flex justify-center items-center py-12 snap-end flex-col relative overflow-hidden w-full">
+        <div className="w-full items-center justify-center flex">
+          <div
+            className={`w-[80%] items-center mobile:justify-center justify-evenly flex gap-8 mobile:gap-4 mobile:flex-col ${
+              full ? 'min-h-[100svh]' : 'h-full'
+            }`}
+            ref={ref}
+          >
+            {children}
+          </div>
+        </div>
+        {showArrow && (
+          <button className="absolute bottom-6 animate-bounce" onClick={scroll}>
+            <Arrow />
+          </button>
+        )}
+      </div>
+    );
+  }
+);
+
+export default SectionLayout;
