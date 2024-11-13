5e43feb081683b3010f553bec51a2f4cf770c72b
d0422 <rlfehd2013@naver.com>
2024-11-13 17:23:56 +0900

    chore: title2 모바일 글꼴 크기 조정

--- src/components/Font.tsx
+++ src/components/Font.tsx
@@ -8,9 +8,9 @@ interface FontProps {
 
 const CLASS_NAME: Record<FontType, string> = {
   title: 'text-[50px] mobile:text-[35px]',
-  title2: 'text-[40px]  mobile:text-[30px] tablet:text-[30px]',
-  introText: 'text-[50px] mobile:text-[20px]  whitespace-pre-wrap',
-  body1: 'text-[25px]  mobile:text-[20px] tablet:text-[30px]',
+  title2: 'text-[40px] mobile:text-[27px] tablet:text-[30px]',
+  introText: 'text-[50px] mobile:text-[20px] whitespace-pre-wrap',
+  body1: 'text-[25px] mobile:text-[20px] tablet:text-[30px]',
   body2: 'text-[20px] mobile:text-[14px]',
 };
 
